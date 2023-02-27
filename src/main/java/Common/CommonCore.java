ce718f27e0f89def81e5970d5a8cc11ccc49a11b
Houseinface <sakovski.dima@gmail.com>
2023-02-28 00:28:51 +0300

    new driver again

--- src/main/java/Common/CommonCore.java
+++ src/main/java/Common/CommonCore.java
@@ -14,7 +14,7 @@ public class CommonCore {
 
         switch (Browser){
             case "Chrome":
-                System.setProperty("webdriver.chrome.driver", "E:\\Tests\\src\\main\\resources\\chromedriver.exe");
+                System.setProperty("webdriver.chrome.driver", "src\\main\\resources\\chromedriver.exe");
                 driver = new ChromeDriver();
                 break;
         }
